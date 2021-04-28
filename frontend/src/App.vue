1b0a85aeda3ada3f46abafde2aa9f9915037f0c5
JudyHopps <judy@JudyHoppsui-MacBookAir.local>
2021-04-28 20:36:57 +0900

    Modify App Layout width 100%

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,7 +15,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 100%;
 }
 
 #nav {
@@ -32,8 +31,10 @@
 }
 
 html,
-body {
+body,
+#app {
   height: 100%;
+  width: 100%;
 }
 
 body {
