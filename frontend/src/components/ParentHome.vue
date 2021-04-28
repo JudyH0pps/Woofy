7684e00bcf9bb7653e01495c7226572b07f80d2b
SY <ssafy_coach_43@ssafy.com>
2021-04-28 16:42:14 +0900

    Add Parent Home Account Section

--- frontend/src/components/ParentHome.vue
+++ frontend/src/components/ParentHome.vue
@@ -1,17 +1,68 @@
 <template>
-  <div>
-    <p>parentchild</p>
-    <router-link :to="{ name: 'ParentChildDetail' }">asdasdsad</router-link>
+  <div class="parentHome">
+    <div class="ParentAccountSummary">
+      <p style="text-align: left">{{ user }}님의 계좌</p>
+      <p style="text-align: right; font-size: 45px; margin: 15% 0">
+        5,000,000 원
+      </p>
+    </div>
+    <div class="ChildAccountSummary" @click="moveTo('ParentChildDetail')">
+      <p style="text-align: left">Woori 아들</p>
+      <p style="text-align: right; font-size: 25px">40,000 원</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      user: "김시영",
+    };
+  },
+  methods: {
+    moveTo(componentName) {
+      this.$router.push({ name: componentName });
+    },
   },
 };
 </script>
 
 <style scoped>
+.parentHome {
+  width: 100%;
+  height: 100%;
+}
+
+.ParentAccountSummary {
+  width: 95%;
+  height: 25%;
+  margin: 10px auto;
+  box-shadow: 2px 2px 2px gray;
+  background-color: rgb(177, 201, 255);
+  display: flex;
+  flex-direction: column;
+}
+
+.ParentAccountSummary > p {
+  margin: 8px;
+  color: rgb(255, 255, 255);
+  font-size: bold;
+}
+
+.ChildAccountSummary {
+  width: 95%;
+  height: 15%;
+  margin: 0 auto;
+  box-shadow: 2px 2px 2px gray;
+  background-color: rgb(232, 239, 255);
+  display: flex;
+  flex-direction: column;
+}
+
+.ChildAccountSummary > p {
+  margin: 8px;
+  color: rgb(63, 62, 62);
+  font-size: bold;
+}
 </style>
