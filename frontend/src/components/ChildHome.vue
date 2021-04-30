5c8a135e8ee891e30709ab26b7d32c354ab0ee37
SY <ssafy_coach_43@ssafy.com>
2021-04-30 15:27:34 +0900

    은행 선택 페이지

--- frontend/src/components/ChildHome.vue
+++ frontend/src/components/ChildHome.vue
@@ -20,18 +20,18 @@
     </div>
     <div class="buttons">
       <v-btn @click="changeViewingContent(0)" plain>결제</v-btn>
-      <v-btn @click="changeViewingContent(1)" plain>송금</v-btn>
+      <v-btn @click="moveTo('ChildRemittance')" plain>송금</v-btn>
       <v-btn @click="changeViewingContent(2)" plain>미션</v-btn>
     </div>
     <ChildPayment v-if="viweingContent === 0"></ChildPayment>
-    <ChildRemittance v-if="viweingContent === 1"></ChildRemittance>
+    <!-- <ChildRemittance v-if="viweingContent === 1"></ChildRemittance> -->
     <ChildMission v-if="viweingContent === 2"></ChildMission>
   </section>
 </template>
 
 <script>
 import ChildPayment from "@/components/ChildPayment.vue";
-import ChildRemittance from "@/components/ChildRemittance.vue";
+// import ChildRemittance from "@/components/ChildRemittance.vue";
 import ChildMission from "@/components/ChildMission.vue";
 export default {
   data() {
@@ -44,10 +44,13 @@ export default {
   },
   components: {
     ChildPayment,
-    ChildRemittance,
+    // ChildRemittance,
     ChildMission,
   },
   methods: {
+    moveTo(component) {
+      this.$router.push({ name: component });
+    },
     changeViewingContent(no) {
       this.viweingContent = no;
     },
