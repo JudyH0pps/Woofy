5d87fff1b37eee7a57db6e43be28effd72778d82
SY <ssafy_coach_43@ssafy.com>
2021-04-30 16:36:20 +0900

    자녀 미션 페이지

--- frontend/src/components/ChildMission.vue
+++ frontend/src/components/ChildMission.vue
@@ -1,5 +1,20 @@
 <template>
-  <section></section>
+  <section class="childMission">
+    <div class="mission">
+      <div style="width: 85%">
+        <p>설거지 하기</p>
+        <p class="amount">+500</p>
+      </div>
+      <v-btn color="primary" fab small>></v-btn>
+    </div>
+    <div class="mission">
+      <div style="width: 85%">
+        <p>시험 100점</p>
+        <p class="amount">+50000</p>
+      </div>
+      <v-btn color="primary" fab small>></v-btn>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -12,4 +27,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.childMission {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.mission {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 95%;
+  border-radius: 5px;
+  border: 3px solid #eee;
+  margin-bottom: 5px;
+  p {
+    margin: 10px;
+  }
+  .amount {
+    font-size: 24px;
+    color: rgb(96, 165, 255);
+    text-align: right;
+    margin-right: 30px;
+  }
+}
 </style>
