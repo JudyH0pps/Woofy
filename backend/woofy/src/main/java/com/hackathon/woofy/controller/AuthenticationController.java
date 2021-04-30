0bc7f47ee7e873dcd58ac90b5902136fa2639856
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-04-30 10:27:37 +0900

    AuthController getCellCerti Implemented with Keydb

--- backend/woofy/src/main/java/com/hackathon/woofy/controller/AuthenticationController.java
+++ backend/woofy/src/main/java/com/hackathon/woofy/controller/AuthenticationController.java
@@ -5,14 +5,17 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,18 +27,25 @@ import com.google.gson.Gson;
 import com.hackathon.woofy.service.ChildService;
 import com.hackathon.woofy.service.MissionService;
 import com.hackathon.woofy.service.ParentService;
+import com.hackathon.woofy.service.RedisService;
+import com.hackathon.woofy.util.SmsFunc;
 import com.hackathon.woofy.util.WooriFunc;
 
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.utility.RandomString;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 	WooriFunc wooriFunc = new WooriFunc();
+	SmsFunc smsFunc = new SmsFunc();
+	
+	@Autowired
+	RedisService redisService;
 	
 	@PostMapping("/getCellCerti")
-	public ResponseEntity<JSONObject> getCellCerti(@RequestBody Map<String, Object> jsonRequest) {
+	public ResponseEntity<JSONObject> getCellCerti(@RequestBody Map<String, Object> jsonRequest) throws ParseException {
 		String requestType = (String)jsonRequest.get("type");
 		Map<String, Object> cellCretiRequestBody = (Map<String, Object>) jsonRequest.get("dataBody");
 		System.out.println(requestType);
@@ -54,33 +64,31 @@ public class AuthenticationController {
 			e.printStackTrace();
 			result = "FAILED";
 		}
+
+		JSONObject responseObject = new JSONObject();
 		
 		if (result.equals("FAILED")) {
-			JSONObject responseObject = new JSONObject();
 			responseObject.put("status", 400);
 			return new ResponseEntity<>(responseObject, HttpStatus.BAD_REQUEST);
 		}
 		
 		// 현재 개발 단계인 관계로 API 요청을 그대로 반환하는 형태이지만 실제로는 다음의 로직을 거친다.
-		
-		// 1. 받아온 CRTF_UTF_NO 값을 가져온다.
-		// 2. 숫자 여섯자리로 구성된 임의의 SMS 인증 코드를 생성한다.
-		// 3. KeyDB(Redis)에 부모키(CellCertiAuth)에 CRTF 값을 Child Key로 지정하고, 임의의 SMS 인증 코드를 값으로 하는 레코드를 생성한다.
-		// 4. RequestBody로부터 휴대전화 번호를 가져온다.
-		// 5. 해당 휴대 전화 번호로 SMS 인증 번호를 첨부하고 SMS를 전송한다.
-		// 6. SMS 전송을 완료했다는 메시지가 담긴 JSONObject를 반환한다. 
-		
+				
 		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(result);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+		JSONObject targetDataBody = (JSONObject) ((JSONObject)parser.parse(result)).get("dataBody");
 		
-		JSONObject jsonObj = (JSONObject) obj;
+		String CRTF_UNQ_NO = (String) targetDataBody.get("CRTF_UNQ_NO");		
+		String SMS_CRTF_NO = RandomStringUtils.randomNumeric(6); // Woori API를 통해 SMS가 전송되어야 하나, 테스트베드에서는 해당 기능이 제한적으로 제공되므로 프로젝트를 위해 임의의 SMS 넘버를 생성하기로 함.
+		String HP_NO = (String)cellCretiRequestBody.get("HP_NO");
+		String targetMessage = "WooFi(가제) SMS 인증번호는 [" + SMS_CRTF_NO + "] 입니다.";
 		
-		return new ResponseEntity<>(jsonObj, HttpStatus.ACCEPTED);
+		redisService.insertHashTableContent("CellCertiTable", SMS_CRTF_NO, CRTF_UNQ_NO);
+		redisService.setHashSetTimeLimit("CellCertiTable", SMS_CRTF_NO, 180);
+		
+		smsFunc.sendMessage(HP_NO, targetMessage);		
+		responseObject.put("status", 200);
+		
+		return new ResponseEntity<>(responseObject, HttpStatus.ACCEPTED);
 	}
 
 	@PostMapping("/executeCellCerti")
@@ -89,6 +97,9 @@ public class AuthenticationController {
 		Map<String, Object> cellCretiRequestBody = (Map<String, Object>) jsonRequest.get("dataBody");
 		System.out.println(requestType);
 		System.out.println(cellCretiRequestBody);
+		
+		JSONObject responseObject = new JSONObject();
+		
 		
 		String result = "";
 		
