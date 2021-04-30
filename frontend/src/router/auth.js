78e76b90e4b95e3878190cab798d007346e97246
SY <ssafy_coach_43@ssafy.com>
2021-04-30 13:41:23 +0900

    Signup 분리

--- frontend/src/router/auth.js
+++ frontend/src/router/auth.js
@@ -1,10 +1,15 @@
 import Login from "@/views/auth/Login.vue";
-
+import Signup from "@/views/auth/Signup.vue";
 const authRouters = [
     {
         path: "/login",
         name: "Login",
         component: Login,
+    },
+    {
+        path: "/signup",
+        name: "Signup",
+        component: Signup,
     },
 ];
 
