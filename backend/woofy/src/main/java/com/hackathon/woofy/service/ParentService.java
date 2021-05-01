08e5b9a7e73258f407ef70e43797a43205fe665e
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-01 11:15:04 +0900

    Adjusted ParentService

--- backend/woofy/src/main/java/com/hackathon/woofy/service/ParentService.java
+++ backend/woofy/src/main/java/com/hackathon/woofy/service/ParentService.java
@@ -34,10 +34,4 @@ public class ParentService {
 	public Parent findByUser(User user) {
 		return parentRepo.findByUser(user);
 	}
-
-	@Transactional
-	public Parent findByUserName(String username) {
-		return parentRepo.findByUsername(username);
-	}
-
 }
