b2b31190c3d0dfd07d0e24dcbf7e6ee06dd332c0
JudyHopps <judy@JudyHoppsui-MacBookAir.local>
2021-05-02 23:41:34 +0900

    실데이터

--- frontend/src/components/ChildMission.vue
+++ frontend/src/components/ChildMission.vue
@@ -1,15 +1,29 @@
 <template>
   <section class="content">
-    <div>매일 미션
-          <Mission class="ParentChildMission" v-for="(mission,index) in dailyMission" :arrow="'right'" :key="index" @missionClick="openModal(mission)" :mission="mission"></Mission>
-      </div>
-        등록된 미션
-          <!-- <v-badge  color="green" :content="1" style="width:90%; margin:0 5%; padding-top:2px; text-align:center" >
+    <div>
+      매일 미션
+      <Mission
+        class="ParentChildMission"
+        v-for="(mission, index) in dailyMission"
+        :arrow="'right'"
+        :key="index"
+        @missionClick="openModal(mission)"
+        :mission="mission"
+      ></Mission>
+    </div>
+    등록된 미션
+    <!-- <v-badge  color="green" :content="1" style="width:90%; margin:0 5%; padding-top:2px; text-align:center" >
           <Mission style="width:100%; margin:0; background: #caf5c975;" class="ParentChildMission" :arrow="'right'" :mission="generalMission[0]"></Mission>         
         </v-badge> -->
-      <Mission class="ParentChildMission" v-for="(mission,index) in generalMission" :arrow="'right'" :key="index" @missionClick="openModal(mission)" :mission="mission"></Mission>
-      <div>
-    </div>
+    <Mission
+      class="ParentChildMission"
+      v-for="(mission, index) in generalMission"
+      :arrow="'right'"
+      :key="index"
+      @missionClick="openModal(mission)"
+      :mission="mission"
+    ></Mission>
+    <div></div>
     <!-- <div class="mission">
       <div style="width: 85%">
         <p>설거지 하기</p>
@@ -48,17 +62,17 @@
 
 <script>
 import { mapActions } from "vuex";
-import Mission from './Mission.vue';
+import Mission from "./Mission.vue";
 
 export default {
-  mounted(){
-    this.missions.forEach(mission => {
-      if(mission.missionStatus == 'REQUEST'){
+  mounted() {
+    this.missions.forEach((mission) => {
+      if (mission.missionStatus == "REQUEST") {
         this.waitingMission.push(mission);
-      }else{
-        if(mission.missionType==0){
+      } else {
+        if (mission.missionType == 0) {
           this.generalMission.push(mission);
-        }else{
+        } else {
           this.dailyMission.push(mission);
         }
       }
@@ -66,67 +80,56 @@ export default {
   },
   data() {
     return {
-      dailyMission:[
+      dailyMission: [],
+      generalMission: [],
+      waitingMission: [],
+      missions: [
+        {
+          title: "설거지하기",
+          content: "미션 설명",
+          prize: 500,
+          missionStatus: "SUCCESS",
+          missionType: 1,
+        },
+        {
+          title: "시험 성적 95점 이상",
+          content: "미션 설명",
+          prize: 50000,
+          missionStatus: "REQUEST",
+          missionType: 0,
+        },
+        {
+          title: "할머니 안마해드리기",
+          content: "미션 설명",
+          prize: 3000,
+          missionStatus: "ONGOING",
+          missionType: 0,
+        },
+        {
+          title: "심부름하기",
+          content: "미션 설명",
+          prize: 1000,
+          missionStatus: "REQUEST",
+          missionType: 0,
+        },
+        {
+          title: "미션 3",
+          content: "미션 설명",
+          prize: 3000,
+          missionStatus: "ONGOING",
+          missionType: 0,
+        },
+        {
+          title: "콩이 산책시키기",
+          content: "미션 설명",
+          prize: 2000,
+          missionStatus: "SUCCESS",
+          missionType: 0,
+        },
       ],
-      generalMission:[
-      ],
-      waitingMission:[
-      ],
-       missions:[
-              {
-                title:'설거지하기',
-                content : '미션 설명',
-                prize : 500,
-                missionStatus:'SUCCESS',
-                missionType:1,
-              },
-              {
-                title:'미션 2',
-                content : '미션 설명',
-                prize : 5000,
-                missionStatus:'REQUEST',
-                missionType:0,
-              },
-              {
-                title:'미션 3',
-                content : '미션 설명',
-                prize : 3000,
-                missionStatus:'ONGOING',
-                missionType:0,
-              },
-              {
-                title:'미션 2',
-                content : '미션 설명',
-                prize : 5000,
-                missionStatus:'REQUEST',
-                missionType:0,
-              },
-              {
-                title:'미션 3',
-                content : '미션 설명',
-                prize : 3000,
-                missionStatus:'ONGOING',
-                missionType:0,
-              },
-              {
-                title:'미션 2',
-                content : '미션 설명',
-                prize : 5000,
-                missionStatus:'SUCCESS',
-                missionType:0,
-
-              },
-              {
-                title:'미션 3',
-                content : '미션 설명',
-                prize : 3000,
-                missionStatus:'ONGOING',
-                missionType:0,
-              },
-            ],
     };
   },
-  components: {Mission},
+  components: { Mission },
   methods: {
     ...mapActions(["moveTo"]),
   },
@@ -159,11 +162,11 @@ export default {
   }
 }
 
-.ParentChildMission{
-    height: 10vh;
-    width: 90%;
-    box-shadow: 0px 0px 4px #00000030;
-    margin: 10px 5%;
-    border-radius: 4px;
+.ParentChildMission {
+  height: 10vh;
+  width: 90%;
+  box-shadow: 0px 0px 4px #00000030;
+  margin: 10px 5%;
+  border-radius: 4px;
 }
 </style>
