3c0f3131458b1d024f6beb5f76228013f10d72de
JudyHopps <judy@JudyHoppsui-MacBookAir.local>
2021-05-03 00:24:11 +0900

    ParentChildDetail 실데이터

--- frontend/src/components/ParentChildDetail.vue
+++ frontend/src/components/ParentChildDetail.vue
@@ -225,28 +225,28 @@ export default {
         ],
         payment: [
           {
-            location: "OOpc방",
-            price: 5000,
-            time: "2021-03-21 16:03:54",
+            location: "CU장덕점",
+            price: 2300,
+            time: "2021-05-02 16:03:54",
           },
           {
-            location: "김밥천국",
-            price: 4500,
+            location: "딜리버리히어로",
+            price: 12000,
+            time: "2021-04-27 18:44:07",
+          },
+          {
+            location: "지바이크",
+            price: 1350,
+            time: "2021-04-21 16:03:54",
+          },
+          {
+            location: "미용실",
+            price: 12000,
             time: "2021-03-21 18:44:07",
           },
           {
-            location: "OOpc방",
-            price: 5000,
-            time: "2021-03-21 16:03:54",
-          },
-          {
-            location: "김밥천국",
-            price: 4500,
-            time: "2021-03-21 18:44:07",
-          },
-          {
-            location: "OOpc방",
-            price: 5000,
+            location: "PC방",
+            price: 2000,
             time: "2021-03-21 16:03:54",
           },
           {
